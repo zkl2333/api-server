a9a4b5c79a7b0944f010608274fbbddfa125c10d
zkl2333 <i@zkl2333.com>
2018-12-17 18:34:21 +0800

    feat: 获取各条河流的数据

--- action/get_api.js
+++ action/get_api.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const axios = require("axios");
+const http = require("http");
 const api = "http://220.191.208.69:8080/ShuiHuanJingFabu/background/app.htm";
 
 async function getData(apiConfig) {
@@ -10,21 +11,26 @@ async function getData(apiConfig) {
 	config = {
 		method: "get",
 		url: api,
+		headers: {
+			'User-Agent':
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
+		},
 		params: {
 			app_sign,
 			app_key: apiConfig ? 1000 : "",
 			...apiConfig,
 			timestamp
-		}
+		},
+		timeout: 6000,
+		httpAgent: new http.Agent({ keepAlive: true }),
 	};
-	await axios(config)
-		.then(function(response) {
-			data = response.data;
-		})
-		.catch(function(error) {
-			console.log(error);
-			data = error.data;
-		});
+	await axios(config).then(function(response) {
+		data = response.data;
+	})
+	.catch(function(error) {
+		console.log(error);
+		throw error;
+	});
 	return data;
 }
 
---
+++ db/replace_riverData.js
@@ -0,0 +1,68 @@
+/** @format */
+
+const getApi = require("../action/get_api");
+const pool = require("./pool");
+const database = require("../db/database");
+
+async function replaceRiverData() {
+	let DistrictLists = await database.getDistrictLists();
+	for (let k in DistrictLists) {
+		console.log("get", DistrictLists[k].districtId);
+		let isError = false;
+		let res;
+		try {
+			res = await getApi({
+				method: "Get_NewRiverSearch_Data",
+				params: {
+					pageSize: 5,
+					currentPage: 1,
+					searchContent: "",
+					searchDistrictId: DistrictLists[k].districtId
+				}
+			});
+		} catch (error) {
+			isError = true;
+			console.log(error);
+			// console.log('网络错误')
+		}
+		if (!isError) {
+			let riverDataList = res.data.riverSums;
+			let velue = "";
+			for (let k in riverDataList) {
+				if (k != 0) velue += ",";
+				velue += `(${riverDataList[k].riverLevel},
+						${riverDataList[k].riverId},
+						"${riverDataList[k].districtName}",
+						${riverDataList[k].districtId},
+						${riverDataList[k].waterType},
+						"${riverDataList[k].riverSerialNum}",
+						"${riverDataList[k].riverPicPath}",
+						"${riverDataList[k].riverName}",
+						${riverDataList[k].ifCare})`;
+			}
+			velue = velue.replace(/ *[\r|\n|\t] */gm, "");
+			if (velue != "") {
+				await new Promise(function(resolve, reject) {
+					try {
+						pool.query(
+							`REPLACE INTO riverData(riverLevel,riverId,districtName,districtId,
+	waterType,riverSerialNum,riverPicPath,riverName,ifCare)VALUES ${velue};`,
+							function(error, results, fields) {
+								resolve(results)
+								if (error) console.log(error.sql, error);
+								console.log("insertId " + results.insertId);
+								console.log("serverStatus" + results.serverStatus);
+							}
+						);
+					} catch (error) {
+						reject(error);
+					}
+				});
+			} else {
+				console.log("空值");
+			}
+		}
+	}
+	console.log("down");
+}
+replaceRiverData();
