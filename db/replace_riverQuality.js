290543f8eae6080cd586f181c54fda05f9d3b18f
zkl2333 <i@zkl2333.com>
2018-12-18 23:29:10 +0800

    feat: 获取水质的脚本

--- db/replace_riverQuality.js
+++ db/replace_riverQuality.js
@@ -5,63 +5,78 @@ const pool = require("./pool");
 const database = require("../db/database");
 
 async function replaceWaterQuality() {
-	let DistrictLists = await database.getDistrictLists();
-	let DistrictLists = await database.getDistrictLists();
-	for (let k in DistrictLists) {
-		console.log("get", DistrictLists[k].districtId);
-		let isError = false;
-		let res;
+	let riverData = await database.getRiverData();
+	for (let k in riverData) {
 		try {
-			res = await getApi({
-				method: "riverwaterquality_data_get",
-				params: {
-					stationId: "HDXH0017",
-					indexId: 2
-				}
-			});
-		} catch (error) {
-			isError = true;
-			console.log(error.messege);
-		}
-		if (!isError) {
-			let riverDataList = res.data.riverSums;
-			let velue = "";
-			for (let k in riverDataList) {
-				if (k != 0) velue += ",";
-				velue += `(${riverDataList[k].riverLevel},
-						${riverDataList[k].riverId},
-						"${riverDataList[k].districtName}",
-						${riverDataList[k].districtId},
-						${riverDataList[k].waterType},
-						"${riverDataList[k].riverSerialNum}",
-						"${riverDataList[k].riverPicPath}",
-						"${riverDataList[k].riverName}",
-						${riverDataList[k].ifCare})`;
+			console.log("get", riverData[k].riverId);
+			let isError = false;
+			let res;
+			let riverId = riverData[k].riverId
+			try {
+				res = await getApi({
+					method: "riverwaterquality_data_get",
+					params: {
+						stationId: riverData[k].riverSerialNum,
+						indexId: 1
+					}
+				});
+			} catch (error) {
+				isError = true;
+				console.log(error.message);
 			}
-			velue = velue.replace(/ *[\r|\n|\t] */gm, "");
-			console.log(velue)
-	// 		if (velue != "") {
-	// 			await new Promise(function(resolve, reject) {
-	// 				try {
-	// 					pool.query(
-	// 						`REPLACE INTO riverData(riverLevel,riverId,districtName,districtId,
-	// waterType,riverSerialNum,riverPicPath,riverName,ifCare)VALUES ${velue};`,
-	// 						function(error, results, fields) {
-	// 							resolve(results);
-	// 							if (error) console.log(error.sql, error);
-	// 							console.log("insertId " + results.insertId);
-	// 							console.log(
-	// 								"serverStatus" + results.serverStatus
-	// 							);
-	// 						}
-	// 					);
-	// 				} catch (error) {
-	// 					reject(error);
-	// 				}
-	// 			});
-	// 		} else {
-	// 			console.log("空值");
-	// 		}
+			if (!isError) {
+				let riverwaterquality
+				let waterLevel
+				let uploadTime
+				let velue = "";
+				try {
+					if (res.data.indexDatas != null) {
+						riverwaterquality = res.data.indexDatas;
+						waterLevel = res.data.waterLevel
+						uploadTime = res.data.uploadTime.time
+					} else {
+						throw new Error('无数据')
+					}
+					for (let k in riverwaterquality) {
+						if (k != 0) velue += ",";
+						velue += `(${riverId},
+						${waterLevel},
+						${uploadTime},
+						${riverwaterquality[k].indexLevel},
+						${riverwaterquality[k].indexValue},
+						'${riverwaterquality[k].indexUnit}',
+						'${riverwaterquality[k].indexNameEN}',
+						'${riverwaterquality[k].indexNameCH}')`;
+					}
+					velue = velue.replace(/ *[\r|\n|\t] */gm, "");
+					// console.log(velue)
+				} catch (error) {
+					console.log(error.message)
+				}
+				if (velue != "") {
+					await new Promise(function (resolve, reject) {
+						try {
+							pool.query(
+								`REPLACE INTO riverQuality(riverId,waterLevel,uploadTime,Level,Value,Unit,NameEN,NameCH) VALUES ${velue};`,
+								function (error, results, fields) {
+									resolve(results);
+									if (error) console.log(error.sql, error);
+									console.log("insertId " + results.insertId);
+									console.log(
+										"serverStatus" + results.serverStatus
+									);
+								}
+							);
+						} catch (error) {
+							reject(error);
+						}
+					});
+				} else {
+					console.log("空值");
+				}
+			}	
+		} catch (error) {
+			console.log(error.message)			
 		}
 	}
 }
