578b9982a01b9649539f43d729286076c8892c53
zkl2333 <i@zkl2333.com>
2018-12-19 12:26:21 +0800

    feat: 使脚本获取历史数据

--- db/replace_riverQuality.js
+++ db/replace_riverQuality.js
@@ -5,41 +5,91 @@ const pool = require("./pool");
 const database = require("../db/database");
 
 async function replaceWaterQuality() {
-	let riverData = await database.getRiverData();
+	try {
+		var riverData = await database.getRiverData();
+	} catch (error) {
+		throw error;
+	}
+	let indexType = [
+		{
+			NameEN: "DO",
+			Unit: "mg/l",
+			NameCH: "溶解氧"
+		},
+		{
+			NameEN: "CODMn",
+			Unit: "mg/l",
+			NameCH: "高锰酸盐指数"
+		},
+		{
+			NameEN: "NH3N",
+			Unit: "mg/l",
+			NameCH: "氨氮"
+		},
+		{
+			NameEN: "TP",
+			Unit: "mg/l",
+			NameCH: "总磷"
+		},
+		{
+			NameEN: "ORP",
+			Unit: "mv",
+			NameCH: "氧化还原电位"
+		},
+		{
+			NameEN: "Transp",
+			Unit: "mg/l",
+			NameCH: "透明度"
+		}
+	];
 	for (let k in riverData) {
-		try {
-			console.log("get", riverData[k].riverId);
-			let isError = false;
-			let res;
-			let riverId = riverData[k].riverId
+		let n = 0; //重试次数
+		for (let i = 0; i < 6; i++) {
 			try {
-				res = await getApi({
-					method: "riverwaterquality_data_get",
-					params: {
-						stationId: riverData[k].riverSerialNum,
-						indexId: 1
-					}
-				});
-			} catch (error) {
-				isError = true;
-				console.log(error.message);
-			}
-			if (!isError) {
-				let riverwaterquality
-				let waterLevel
-				let uploadTime
-				let velue = "";
+				console.log(
+					"获取",
+					riverData[k].riverId,
+					riverData[k].riverName,
+					i == 0 ? "六项水质，以及" : "",
+					indexType[i].NameCH + " 的历史数据"
+				);
+				let res;
+				let riverId = riverData[k].riverId;
 				try {
-					if (res.data.indexDatas != null) {
-						riverwaterquality = res.data.indexDatas;
-						waterLevel = res.data.waterLevel
-						uploadTime = res.data.uploadTime.time
+					//获取api
+					res = await getApi({
+						method: "riverwaterquality_data_get",
+						params: {
+							stationId: riverData[k].riverSerialNum,
+							indexId: i + 1
+						}
+					});
+				} catch (error) {
+					i--;
+					// console.log(error.message);
+					if (n < 5) {
+						console.log("重试", ++n, "次");
 					} else {
-						throw new Error('无数据')
+						console.log("放弃");
+						i++;
+						n = 0;
 					}
-					for (let k in riverwaterquality) {
-						if (k != 0) velue += ",";
-						velue += `(${riverId},
+					throw error;
+				}
+				try {
+					//插入数据
+					try {
+						var velue = "";
+						var riverwaterquality = res.data.indexDatas;
+						var waterLevel = res.data.waterLevel;
+						var uploadTime = res.data.uploadTime.time;
+					} catch {
+						throw new Error("无数据");
+					}
+					if (i == 0) {
+						for (let k in riverwaterquality) {
+							if (k != 0) velue += ",";
+							velue += `(${riverId},
 						${waterLevel},
 						${uploadTime},
 						${riverwaterquality[k].indexLevel},
@@ -47,23 +97,37 @@ async function replaceWaterQuality() {
 						'${riverwaterquality[k].indexUnit}',
 						'${riverwaterquality[k].indexNameEN}',
 						'${riverwaterquality[k].indexNameCH}')`;
+						}
+					}
+
+					let indexValues = res.data.indexValues;
+					for (let k in indexValues) {
+						if (i == 0 || k != 0) velue += ",";
+						velue += `(${riverId},
+						null,
+						${indexValues[k].getTime.time},
+						null,
+						${indexValues[k].indexValue},
+						'${indexType[i].Unit}',
+						'${indexType[i].NameEN}',
+						'${indexType[i].NameCH}')`;
 					}
 					velue = velue.replace(/ *[\r|\n|\t] */gm, "");
-					// console.log(velue)
 				} catch (error) {
-					console.log(error.message)
+					console.log("\t>数据解析错误");
+					throw error;
 				}
-				if (velue != "") {
-					await new Promise(function (resolve, reject) {
+				try {
+					await new Promise(function(resolve, reject) {
 						try {
 							pool.query(
 								`REPLACE INTO riverQuality(riverId,waterLevel,uploadTime,Level,Value,Unit,NameEN,NameCH) VALUES ${velue};`,
-								function (error, results, fields) {
+								function(error, results, fields) {
 									resolve(results);
 									if (error) console.log(error.sql, error);
-									console.log("insertId " + results.insertId);
 									console.log(
-										"serverStatus" + results.serverStatus
+										"\t>数据插入成功\n\t>",
+										results.message
 									);
 								}
 							);
@@ -71,15 +135,20 @@ async function replaceWaterQuality() {
 							reject(error);
 						}
 					});
-				} else {
-					console.log("空值");
+				} catch (error) {
+					console.log("数据库错误");
+					throw error;
 				}
-			}	
-		} catch (error) {
-			console.log(error.message)			
+			} catch (error) {
+				console.log("\t>" + error.message);
+			}
 		}
 	}
 }
-replaceWaterQuality().then(() => {
-	console.log("down");
-});
+replaceWaterQuality()
+	.then(() => {
+		console.log("运行结束");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
